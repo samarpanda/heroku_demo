e8f5661ae31aad823d47454969e7805df766ac0d
Samar Panda <samar@stepout.com>
2015-02-17 05:41:16 +0530

    PotgreSQL demo added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +12,20 @@ app.get('/', function(request, response) {
   for (var i = 0; i < times; i++)
   	result += cool();
   response.send(result);
+});
+
+app.get('/db', function(request, response){
+	pg.connect(process.env.DATABASE_URL, function(err, client, done){
+		client.query('SELECT * FROM test_table', function(err, result){
+			done();
+			if(err){
+				console.log(err);
+				response.send("Error "+err);
+			}else{
+				response.send(result.rows);
+			}
+		});
+	});
 });
 
 app.listen(app.get('port'), function() {
